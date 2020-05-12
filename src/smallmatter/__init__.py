fdf8a74bb5adb49ccb338d137beeedf48288923e
Verdi March <marcverd@amazon.com>
2020-05-12 22:47:57 +0800

    Prepare v0.0.1

--- src/smallmatter/__init__.py
+++ src/smallmatter/__init__.py
@@ -1,2 +1,16 @@
 # noqa
 __all__ = ["ds", "pathlib"]
+
+import pathlib
+
+import pkg_resources
+
+_pkg_dir: pathlib.Path = pathlib.Path(__file__).resolve().parent
+
+try:
+    # Loading installed module, so read the installed version
+    __version__ = pkg_resources.require(_pkg_dir.name)[0].version
+except pkg_resources.DistributionNotFound:
+    # Loading uninstalled module, so try to read version from ../../VERSION.
+    with open(_pkg_dir / ".." / ".." / "VERSION", "r") as f:
+        __version__ = f.readline().strip()
